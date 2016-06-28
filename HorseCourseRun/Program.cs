ba4c7cd1eb38e88b6c8f27b91b4dfe75169e0aa4
Goshca <goshca2005@yandex.ru>
2016-06-28 15:08:52 +0300

    first_step

--- HorseCourseRun/Program.cs
+++ HorseCourseRun/Program.cs
@@ -10,6 +10,26 @@ namespace HorseCourseRun
     {
         static void Main(string[] args)
         {
+            int FieldHor = 0;
+            int FieldVer = 0;
+            Console.WriteLine("-=Enter the size of the field=-");
+            Console.Write("Value horizontals: ");
+            FieldHor = int.Parse(Console.ReadLine());
+            Console.Write("Value vertical: ");
+            FieldVer = int.Parse(Console.ReadLine());
+            Console.WriteLine("Playing the Field: " + FieldHor + "x" + FieldVer);
+            string[,] Field = new string[FieldVer, FieldHor];
+            for (int i = 0; i < FieldVer; i++)
+            {
+                for (int j = 0; j < FieldHor; j++)
+                {
+                    Field[i, j] = "o";
+                    Console.Write(Field[i, j] + " ");
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
         }
     }
 }
